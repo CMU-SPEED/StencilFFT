a529a898615336cefd3d17bc5fe1657d45841e40
Christopher Stange <cjstange@andrew.cmu.edu>
2025-11-02 23:54:12 -0500

    begin zmodel fft cleanup

--- zmodel/utils.h
+++ zmodel/utils.h
@@ -1,49 +1,57 @@
-#include <complex>
-#include <cufft.h>
-#include <cuda_runtime.h>
+#ifndef __ZMODEL__UTILS__
+#define __ZMODEL__UTILS__
+
+#define __ZMODEL__CUDA__
+// #define __ZMODEL__HIP__
+#include "device_macros.h"
+
+#define __PRINT__TIMING__
+#define __PRINT__RESULTS__
+// #define __PRINT__TWIDDLES__
+// #define __PRINT__SANITY__
 
 // FIXME: need to revist the use of macros --> we want to be able to pass things in on commandline
-#define N (64)
-#define b (4)
-#define p (2) // Number of processors in the rows or columns. ex: p = 2 --> 4 total processors
-#define local (N/p)
+#define N_DIM (64)
+#define B_DIM (4)
+#define P_DIM (2) // Number of processors in the rows or columns. ex: p = 2 --> 4 total processors
+#define LOCAL_DIM (N_DIM/P_DIM)
+#define LOCAL_BYTES (LOCAL_DIM*LOCAL_DIM*sizeof(Complex))
 
 // This is the size of the vector the stencil will be batched with
-#define vec (local/b)
+#define VEC (LOCAL_DIM/B_DIM)
 // The total number of vectors on the local processor
-#define vec_total ((local*local)/vec)
+#define VEC_TOTAL ((LOCAL_DIM*LOCAL_DIM)/VEC)
  
-#define vec_row (local) // Number of vectors in a col
-#define vec_col (b)     // Number of vectors in a row
+#define VEC_ROW (LOCAL_DIM) // Number of vectors in a col
+#define VEC_COL (B_DIM)     // Number of vectors in a row
 
-// using Complex = std::complex<double>;
-using Complex = cufftDoubleComplex;
+using Complex = DEVICE_FFT_DOUBLECOMPLEX;
 
 // FIXME: this need to be revisted --> I think cid and rid may be swapped
-void init_host(int rid, int cid, int local_size, Complex *host_in) {
-    int row_offset = rid * b;       // offset based on which processor in the row
-    int col_offset = cid * (N*b);   // offset based on which processor in the col
+void init_host(int rid, int cid, Complex *host_in) {
+    int row_offset = rid * B_DIM;           // offset based on which processor in the row
+    int col_offset = cid * (N_DIM*B_DIM);   // offset based on which processor in the col
     int offset = row_offset + col_offset;
 
-    Complex *init = (Complex*)malloc(local_size);
+    Complex *init = (Complex*)malloc(LOCAL_BYTES);
     
-    for (int i = 0; i < local/b; i++) {
-        for (int ii = 0; ii < b; ii++) {
-            for (int j = 0; j < local/b; j++) {
-                for (int jj = 0; jj < b; jj++) {
-                    int array_index = (i * b * (local/b) * b) + (ii * (local/b) * b) + (j * b) + jj;
-                    double real = (i * N * b * p) + (ii * N) + (j * b * p) + jj + offset;
+    for (int i = 0; i < LOCAL_DIM/B_DIM; i++) {
+        for (int ii = 0; ii < B_DIM; ii++) {
+            for (int j = 0; j < LOCAL_DIM/B_DIM; j++) {
+                for (int jj = 0; jj < B_DIM; jj++) {
+                    int array_index = (i * B_DIM * (LOCAL_DIM/B_DIM) * B_DIM) + (ii * (LOCAL_DIM/B_DIM) * B_DIM) + (j * B_DIM) + jj;
+                    double real = (i * N_DIM * B_DIM * P_DIM) + (ii * N_DIM) + (j * B_DIM * P_DIM) + jj + offset;
                     init[array_index] = {real, 0.0};
                 }
             }
         }
     }
 
-    for (int i = 0; i < local; i++) {
-        for (int j = 0; j < b; j++) {
-            for (int jj = 0; jj < local/b; jj++) {
-                int row = i * local;
-                host_in[row + (j * (local/b))+ jj] = init[row + j + (jj * b)];
+    for (int i = 0; i < LOCAL_DIM; i++) {
+        for (int j = 0; j < B_DIM; j++) {
+            for (int jj = 0; jj < LOCAL_DIM/B_DIM; jj++) {
+                int row = i * LOCAL_DIM;
+                host_in[row + (j * (LOCAL_DIM/B_DIM))+ jj] = init[row + j + (jj * B_DIM)];
             }
         }
     }
@@ -51,74 +59,75 @@ void init_host(int rid, int cid, int local_size, Complex *host_in) {
     free(init);
 }
 
-
-void init_plans(cufftHandle *plan0, cufftHandle *plan1, cufftHandle *plan2, cufftHandle *plan3) {
-    cufftCreate(plan0); // FIXME: need to wrap this in a check
-    cufftCreate(plan1);
-    cufftCreate(plan2);
-    cufftCreate(plan3);
+void init_forward_plans(cufftHandle *plan0, cufftHandle *plan1, cufftHandle *plan2, cufftHandle *plan3) {
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_CREATE(plan0));
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_CREATE(plan1));
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_CREATE(plan2));
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_CREATE(plan3));
 
     const int rank0 = 1;
-    int n0[1] = { vec };
-    int inembed0[1] = { vec };
-    int onembed0[1] = { vec };
+    int n0[1] = { VEC };
+    int inembed0[1] = { VEC };
+    int onembed0[1] = { VEC };
     const int istride0 = 1;
     const int ostride0 = 1;
-    const int idist0 = vec;
-    const int odist0 = vec;
-    const int batch0 = vec_total;
+    const int idist0 = VEC;
+    const int odist0 = VEC;
+    const int batch0 = VEC_TOTAL;
     size_t workSize0 = 0;
-    cufftMakePlanMany(
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_MAKE_PLAN_MANY(
       *plan0, rank0, n0,
       inembed0,  istride0, idist0,
       onembed0,  ostride0, odist0,
-      CUFFT_Z2Z, batch0, &workSize0);
+      DEVICE_FFT_Z2Z, batch0, &workSize0));
 
     const int rank1 = 1;
-    int n1[1] = { local/(vec/p) };
-    int inembed1[1] = { local };
-    int onembed1[1] = { local };
-    const int istride1 = vec/p;
-    const int ostride1 = vec/p;
+    int n1[1] = { LOCAL_DIM/(VEC/P_DIM) };
+    int inembed1[1] = { LOCAL_DIM };
+    int onembed1[1] = { LOCAL_DIM };
+    const int istride1 = VEC/P_DIM;
+    const int ostride1 = VEC/P_DIM;
     const int idist1 = 1;
     const int odist1 = 1;
-    const int batch1 = vec/p;
+    const int batch1 = VEC/P_DIM;
     size_t workSize1 = 0;
-    cufftMakePlanMany(
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_MAKE_PLAN_MANY(
       *plan1, rank1, n1,
       inembed1,  istride1, idist1,
       onembed1,  ostride1, odist1,
-      CUFFT_Z2Z, batch1, &workSize1);
+      DEVICE_FFT_Z2Z, batch1, &workSize1));
 
     const int rank2 = 1;
-    int n2[1] = { vec };
-    int inembed2[1] = { local*local };
-    int onembed2[1] = { local*local };
-    const int istride2 = local*b;
-    const int ostride2 = local*b;
+    int n2[1] = { VEC };
+    int inembed2[1] = { LOCAL_DIM*LOCAL_DIM };
+    int onembed2[1] = { LOCAL_DIM*LOCAL_DIM };
+    const int istride2 = LOCAL_DIM*B_DIM;
+    const int ostride2 = LOCAL_DIM*B_DIM;
     const int idist2 = 1;
     const int odist2 = 1;
-    const int batch2 = local*b;
+    const int batch2 = LOCAL_DIM*B_DIM;
     size_t workSize2 = 0;
-    cufftMakePlanMany(
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_MAKE_PLAN_MANY(
       *plan2, rank2, n2,
       inembed2,  istride2, idist2,
       onembed2,  ostride2, odist2,
-      CUFFT_Z2Z, batch2, &workSize2);
+      DEVICE_FFT_Z2Z, batch2, &workSize2));
 
     const int rank3 = 1;
-    int n3[1] = { b*p };
-    int inembed3[1] = { local*b*p };
-    int onembed3[1] = { local*b*p };
-    const int istride3 = local;
-    const int ostride3 = local;
+    int n3[1] = { B_DIM*P_DIM };
+    int inembed3[1] = { LOCAL_DIM*B_DIM*P_DIM };
+    int onembed3[1] = { LOCAL_DIM*B_DIM*P_DIM };
+    const int istride3 = LOCAL_DIM;
+    const int ostride3 = LOCAL_DIM;
     const int idist3 = 1;
     const int odist3 = 1;
-    const int batch3 = local;
+    const int batch3 = LOCAL_DIM;
     size_t workSize3 = 0;
-    cufftMakePlanMany(
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_MAKE_PLAN_MANY(
       *plan3, rank3, n3,
       inembed3,  istride3, idist3,
       onembed3,  ostride3, odist3,
-      CUFFT_Z2Z, batch3, &workSize3);
+      DEVICE_FFT_Z2Z, batch3, &workSize3));
 }
+
+#endif
--- zmodel/zmodel_fft.cu
+++ zmodel/zmodel_fft.cu
@@ -1,232 +1,259 @@
 #include <mpi.h>
 #include "utils.h"
-#include <iostream>
 #include <chrono>
 
-// FIXME: use device macros
+__global__ void pack_forward_all_to_all_row(Complex *device_in, Complex *device_out) {
+    const int row = blockIdx.x / VEC_COL;
+    const int col = blockIdx.x % VEC_COL;
+    const int in_offset = (row * LOCAL_DIM) + (col * VEC);
 
-__global__ void pack_all_to_all_row(Complex *device_in, Complex *device_out) {
-    const int row = blockIdx.x / vec_col;
-    const int col = blockIdx.x % vec_col;
-    const int in_offset = (row * local) + (col * vec);
+    const int vec_index = threadIdx.x;                      // Index into the vector
 
-    const int vec_index = threadIdx.x;        // Index into the vector
-
-    const int vec_split = vec / p;            // The size of a split vector
-    const int split = vec_index / vec_split;  // Which section of the split vector
-    const int index_in_split = vec_index % vec_split; // Index into the split vector
+    const int vec_split = VEC / P_DIM;                      // The size of a split vector
+    const int split = vec_index / vec_split;                // Which section of the split vector
+    const int index_in_split = vec_index % vec_split;       // Index into the split vector
 
     // Note these are both measured in element space
-    const int slab_size = (local*local) / p;      // Num elements in a slab of splits
-    const int slab_vec_row = local / p;           // Number of split vectors in a col of a slab
-    const int slab_vec_col = local / vec_split;   // Number of split vectors in a row of a slab/local
+    const int slab_size = (LOCAL_DIM*LOCAL_DIM) / P_DIM;    // Num elements in a slab of splits
+    const int slab_vec_row = LOCAL_DIM / P_DIM;             // Number of split vectors in a col of a slab
+    const int slab_vec_col = LOCAL_DIM / vec_split;         // Number of split vectors in a row of a slab/local
 
     const int slab_offset = split * slab_size;
     const int out_row = (blockIdx.x / slab_vec_col) % slab_vec_row;
     const int out_col = blockIdx.x % slab_vec_col;
-    const int out_offset = slab_offset + (out_row * local) + (out_col * vec_split);
+    const int out_offset = slab_offset + (out_row * LOCAL_DIM) + (out_col * vec_split);
 
     device_out[out_offset + index_in_split] = device_in[in_offset + vec_index];
 }
 
-__global__ void pack_fft_1(Complex *device_in, Complex *device_out) {
-    const int split_size = local / p;                            // The size of the split
+__global__ void pack_forward_fft0(Complex *device_in, Complex *device_out) {
+    const int split_size = LOCAL_DIM / P_DIM;                // The size of the split
 
     const int index_in_row_split = blockIdx.x % split_size;
     const int index_in_vec_split = threadIdx.x % split_size;
     
-    const int row_split = blockIdx.x / split_size;       // Which section of the row split
-    const int vec_split = threadIdx.x / split_size;      // Which section of the split local vector
+    const int row_split = blockIdx.x / split_size;          // Which section of the row split
+    const int vec_split = threadIdx.x / split_size;         // Which section of the split local vector
 
     // Swap the vec and row splits
-    const int row = vec_split + (index_in_row_split * p);
+    const int row = vec_split + (index_in_row_split * P_DIM);
     const int col = (row_split * split_size) + index_in_vec_split;
 
-    device_out[row * local + col] = device_in[blockIdx.x * local + threadIdx.x];
+    device_out[row * LOCAL_DIM + col] = device_in[blockIdx.x * LOCAL_DIM + threadIdx.x];
 }
 
-__global__ void pack_fft_2(Complex *device_in, Complex *device_out) {
-    const int split_size = local / p;
+__global__ void pack_forward_fft1(Complex *device_in, Complex *device_out) {
+    const int split_size = LOCAL_DIM / P_DIM;
 
     const int which_split = blockIdx.x / split_size;
     const int index_in_split = blockIdx.x % split_size;
 
-    const int which_block = index_in_split / b;
-    const int index_in_block = index_in_split % b;
+    const int which_block = index_in_split / B_DIM;
+    const int index_in_block = index_in_split % B_DIM;
 
-    const int row = (which_block * p * b) + (which_split * b) + index_in_block;
+    const int row = (which_block * P_DIM * B_DIM) + (which_split * B_DIM) + index_in_block;
 
-    device_out[row * local + threadIdx.x] = device_in[blockIdx.x * local + threadIdx.x];
+    device_out[row * LOCAL_DIM + threadIdx.x] = device_in[blockIdx.x * LOCAL_DIM + threadIdx.x];
 }
 
 // FIXME: apply the twiddles in the packing routines
+// FIXME: use PRINT_TWIDDLES in twiddles init
+// FIXME" use APPLY_TWIDDLES MACRO
 
-void init_twiddles0(Complex *in, int cid) {
-    for (int local_row = 0; local_row < local; local_row++) {
-        for (int fft_row = 0; fft_row < b; fft_row++) {
-            for (int fft_col = 0; fft_col < local/b; fft_col++) {
-                int fft_row_offset = cid * b;                           // FIXME: is b the correct offet here?
+void init_forward_twiddles0(Complex *in, int cid) {
+    for (int local_row = 0; local_row < LOCAL_DIM; local_row++) {
+        for (int fft_row = 0; fft_row < B_DIM; fft_row++) {
+            for (int fft_col = 0; fft_col < LOCAL_DIM/B_DIM; fft_col++) {
+                int fft_row_offset = cid * B_DIM;                           // FIXME: is b the correct offet here?
                 double k = (double)(fft_row + fft_row_offset);
                 double l = (double)fft_col;
-                in[(local_row * local) + fft_row + (fft_col * b)] = {k, l};
+                in[(local_row * LOCAL_DIM) + fft_row + (fft_col * B_DIM)] = {k, l};
                 // in[(local_row * local) + fft_row + (fft_col * b)] = std::exp(Complex(0.0, -2*M_PI*k*l/local));
             }
         }
     }
 }
 
-void init_twiddles1(Complex *in, int rid) {
-    for (int fft_row = 0; fft_row < (local*local) / (local*b*p); fft_row++) {
-        for (int fft_col = 0; fft_col < b*p; fft_col++) {
-            for (int local_col = 0; local_col < local; local_col++) {
-                int fft_row_offset = rid * b;                       // FIXME: i dont think this is correct
+void init_forward_twiddles1(Complex *in, int rid) {
+    for (int fft_row = 0; fft_row < (LOCAL_DIM*LOCAL_DIM) / (LOCAL_DIM*B_DIM*P_DIM); fft_row++) {
+        for (int fft_col = 0; fft_col < B_DIM*P_DIM; fft_col++) {
+            for (int local_col = 0; local_col < LOCAL_DIM; local_col++) {
+                int fft_row_offset = rid * B_DIM;                       // FIXME: i dont think this is correct
                 double k = (double)(fft_row + fft_row_offset);
                 double l = (double)fft_col;
-                in[fft_row * (local*b*p) + fft_col * local + local_col] = {k , l};
+                in[fft_row * (LOCAL_DIM*B_DIM*P_DIM) + fft_col * LOCAL_DIM + local_col] = {k , l};
             }
         }
     }
+}
+
+/**
+ * @brief 
+ * 
+
+ Data starts in device_buf0
+ Data ends in device_buf0
+
+ Host buffers and memcopy are needed because we dont have GPU aware MPI
+
+ NULL pointer checks
+
+ Check pointer function declaration convention
+ */
+void forward_fft(Complex *host_buf0, Complex *host_buf1, Complex *device_buf0, Complex *device_buf1,
+                 Complex *device_twiddles0, Complex *device_twiddles1, MPI_Comm *row_comm, MPI_Comm *col_comm,
+                 cufftHandle *plan0, cufftHandle *plan1, cufftHandle *plan2, cufftHandle *plan3) {
+
+    #ifdef __PRINT__TIMING__
+    MPI_Barrier(MPI_COMM_WORLD);
+    auto start = std::chrono::high_resolution_clock::now();
+    #endif
+
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_EXECZ2Z(*plan0, device_buf0, device_buf1, DEVICE_FFT_FORWARD));
+
+    pack_forward_all_to_all_row<<<VEC_TOTAL, VEC>>>(device_buf1, device_buf0);
+    DEVICE_RT_SAFE_CALL(DEVICE_SYNCHRONIZE());
+
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(host_buf1, device_buf0, LOCAL_BYTES, MEM_COPY_DEVICE_TO_HOST));
+
+    MPI_Alltoall(host_buf1,
+                (LOCAL_DIM*LOCAL_DIM)/P_DIM,
+                MPI_C_DOUBLE_COMPLEX,
+                host_buf0, 
+                (LOCAL_DIM*LOCAL_DIM)/P_DIM,
+                MPI_C_DOUBLE_COMPLEX,
+                *row_comm);
+
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(device_buf0, host_buf0, LOCAL_BYTES, MEM_COPY_HOST_TO_DEVICE));
+
+    pack_forward_fft0<<<LOCAL_DIM, LOCAL_DIM>>>(device_buf0, device_buf1);
+    DEVICE_RT_SAFE_CALL(DEVICE_SYNCHRONIZE());
+
+    // FIXME: can we parallelize this with streams --> benchmark to see if this is a bottleneck first
+    for (int row = 0; row < LOCAL_DIM; row++) {
+        Complex* row_ptr0 = device_buf1 + row * LOCAL_DIM;
+        Complex* row_ptr1 = device_buf0+ row * LOCAL_DIM;
+        DEVICE_FFT_SAFE_CALL(DEVICE_FFT_EXECZ2Z(*plan1, row_ptr0, row_ptr1, DEVICE_FFT_FORWARD));
+    }
+
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_EXECZ2Z(*plan2, device_buf0, device_buf1, DEVICE_FFT_FORWARD));
+
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(host_buf1, device_buf1, LOCAL_BYTES, MEM_COPY_DEVICE_TO_HOST));
+
+    MPI_Alltoall(host_buf1,
+                (LOCAL_DIM*LOCAL_DIM)/P_DIM,
+                MPI_C_DOUBLE_COMPLEX,
+                host_buf0,
+                (LOCAL_DIM*LOCAL_DIM)/P_DIM,
+                MPI_C_DOUBLE_COMPLEX,
+                *col_comm);
+
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(device_buf0, host_buf0, LOCAL_BYTES, MEM_COPY_HOST_TO_DEVICE));
+
+    pack_forward_fft1<<<LOCAL_DIM, LOCAL_DIM>>>(device_buf0, device_buf1);
+    DEVICE_RT_SAFE_CALL(DEVICE_SYNCHRONIZE());
+
+    for (int row = 0; row < LOCAL_DIM/(P_DIM*B_DIM); row++) {
+        Complex* row_ptr0 = device_buf1 + row * LOCAL_DIM * P_DIM * B_DIM;
+        Complex* row_ptr1 = device_buf0 + row * LOCAL_DIM * P_DIM * B_DIM;
+        DEVICE_FFT_SAFE_CALL(DEVICE_FFT_EXECZ2Z(*plan3, row_ptr0, row_ptr1, DEVICE_FFT_FORWARD));
+    }
+
+    #ifdef __PRINT__TIMING__
+    MPI_Barrier(MPI_COMM_WORLD);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    double ns = duration.count();
+    std::cout << "FFT: " << ns << " ns" << std::endl;
+    #endif
 }
 
 int main(void) {
     MPI_Init(NULL, NULL);
 
     int P, id;
-    P = p * p;
+    P = P_DIM * P_DIM;
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &P);
 
+    #ifdef __PRINT__SANITY__
+    if (id == 0) std::cout << "N_DIM:" << N_DIM << ", B_DIM:" << B_DIM << " P_DIM:" << P_DIM << std::endl;
+    #endif
+
     MPI_Comm row_comm, col_comm;
 
-    int rid = id / p;
+    int rid = id / P_DIM;
     MPI_Comm_split(MPI_COMM_WORLD, rid, id, &row_comm);
 
-    int cid = id % p;
+    int cid = id % P_DIM;
     MPI_Comm_split(MPI_COMM_WORLD, cid, id, &col_comm);
 
     cufftHandle plan0, plan1, plan2, plan3;
-    init_plans(&plan0, &plan1, &plan2, &plan3);
+    init_forward_plans(&plan0, &plan1, &plan2, &plan3);
 
-    int local_size = local*local*sizeof(Complex);
+    Complex *host_buf0, *host_buf1, *host_twiddles0, *host_twiddles1;
+    DEVICE_RT_SAFE_CALL(DEVICE_HOST_ALLOC((void**)&host_buf0, LOCAL_BYTES, DEVICE_HOST_ALLOC_DEFAULT));
+    DEVICE_RT_SAFE_CALL(DEVICE_HOST_ALLOC((void**)&host_buf1, LOCAL_BYTES, DEVICE_HOST_ALLOC_DEFAULT));
+    DEVICE_RT_SAFE_CALL(DEVICE_HOST_ALLOC((void**)&host_twiddles0, LOCAL_BYTES, DEVICE_HOST_ALLOC_DEFAULT));
+    DEVICE_RT_SAFE_CALL(DEVICE_HOST_ALLOC((void**)&host_twiddles1, LOCAL_BYTES, DEVICE_HOST_ALLOC_DEFAULT));
+    init_host(cid, rid, host_buf0);                                                                             // FIXME: the rid and cid args are swapped in this
+    init_forward_twiddles0(host_twiddles0, cid);
+    init_forward_twiddles1(host_twiddles1, rid);
 
-    // FIXME: make these cuda malloc
-    Complex *host_in = (Complex*)malloc(local_size);
-    Complex *host_out = (Complex*)calloc(local*local, sizeof(Complex));
-    Complex *twiddles0 = (Complex*)calloc(local*local, sizeof(Complex));
-    Complex *twiddles1 = (Complex*)calloc(local*local, sizeof(Complex));
-
-    init_host(cid, rid, local_size, host_in);   // FIXME: the rid and cid args are swapped in this
-    init_twiddles0(twiddles0, cid);
-    init_twiddles1(twiddles1, rid);
-
+    #ifdef __PRINT__TWIDDLES__
     if (id == 0) {
         std::cout << "Twiddles" << std::endl;
         std::cout << "rid: " << rid << " " << "cid: " << cid << std::endl;
-        for (int i = 0; i < local; i++) {
-            for (int j = 0; j < local; j++) {
-                std::cout << "(" << twiddles1[i * local + j].x << ", " << twiddles1[i * local + j].y << ") ";
+        for (int i = 0; i < LOCAL_DIM; i++) {
+            for (int j = 0; j < LOCAL_DIM; j++) {
+                std::cout << "(" << host_twiddles1[i * LOCAL_DIM + j].x << ", " << host_twiddles1[i * LOCAL_DIM + j].y << ") ";
             }
             std::cout << std::endl;
         }
     }
+    #endif
 
-    Complex *device_in, *device_out;
-    cudaMalloc((void**)&device_in, local_size);
-    cudaMalloc((void**)&device_out, local_size);
+    Complex *device_buf0, *device_buf1, *device_twiddles0, *device_twiddles1;
+    DEVICE_RT_SAFE_CALL(DEVICE_MALLOC((void**)&device_buf0, LOCAL_BYTES));
+    DEVICE_RT_SAFE_CALL(DEVICE_MALLOC((void**)&device_buf1, LOCAL_BYTES));
+    DEVICE_RT_SAFE_CALL(DEVICE_MALLOC((void**)&device_twiddles0, LOCAL_BYTES));
+    DEVICE_RT_SAFE_CALL(DEVICE_MALLOC((void**)&device_twiddles1, LOCAL_BYTES));
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(device_buf0, host_buf0, LOCAL_BYTES, MEM_COPY_HOST_TO_DEVICE));
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(device_buf1, host_buf1, LOCAL_BYTES, MEM_COPY_HOST_TO_DEVICE));
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(device_twiddles0, host_twiddles0, LOCAL_BYTES, MEM_COPY_HOST_TO_DEVICE));
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(device_twiddles1, host_twiddles0, LOCAL_BYTES, MEM_COPY_HOST_TO_DEVICE));
 
-    auto start = std::chrono::high_resolution_clock::now();
+    forward_fft(host_buf0, host_buf1, device_buf0, device_buf1,
+                 device_twiddles0, device_twiddles1, 
+                 &row_comm, &col_comm, &plan0, &plan1, &plan2, &plan3);
 
-    cudaMemcpy(device_in, host_in, local_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(device_out, host_out, local_size, cudaMemcpyHostToDevice);
+    DEVICE_RT_SAFE_CALL(DEVICE_MEM_COPY(host_buf1, device_buf0, LOCAL_BYTES, MEM_COPY_DEVICE_TO_HOST));
 
-    // FIXME: Doing this in place might be slower?
-    // cufftExecZ2Z(plan0, device_in, device_in, CUFFT_FORWARD);
-
-    pack_all_to_all_row<<<vec_total, vec>>>(device_in, device_out);
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(host_out, device_out, local_size, cudaMemcpyDeviceToHost);
-
-    // Twiddles
-
-    MPI_Alltoall(host_out,
-                (local * local) / p,
-                MPI_C_DOUBLE_COMPLEX,
-                host_in, 
-                (local * local) / p,
-                MPI_C_DOUBLE_COMPLEX,
-                row_comm);
-
-    // for (int i = 0; i < local; i++)
-    //     for (int j = 0; j< local; j++)
-    //         host_out[i * local + j] = 0.0;
-    
-    cudaMemcpy(device_in, host_in, local_size, cudaMemcpyHostToDevice);
-    // cudaMemcpy(device_out, host_out, local_size, cudaMemcpyHostToDevice); // FIXME: remove, this is for debug
-
-    pack_fft_1<<<local, local>>>(device_in, device_out);
-    cudaDeviceSynchronize();
-
-    // FIXME: can we parallelize this
-    // for (int row = 0; row < local; row++) {
-    //     Complex* row_ptr = device_out + row * local;
-    //     cufftExecZ2Z(plan1, row_ptr, row_ptr, CUFFT_FORWARD);
-    // }
-
-    // cufftExecZ2Z(plan2, device_out, device_out, CUFFT_FORWARD);
-
-    cudaMemcpy(host_out, device_out, local_size, cudaMemcpyDeviceToHost);
-
-    MPI_Alltoall(host_out,
-                (local * local) / p,
-                MPI_C_DOUBLE_COMPLEX,
-                host_in,
-                (local * local) / p,
-                MPI_C_DOUBLE_COMPLEX,
-                col_comm);
-
-    // for (int i = 0; i < local; i++)
-    //     for (int j = 0; j< local; j++)
-    //         host_out[i * local + j] = 0.0;
-    
-    cudaMemcpy(device_in, host_in, local_size, cudaMemcpyHostToDevice);
-    // cudaMemcpy(device_out, host_out, local_size, cudaMemcpyHostToDevice); // FIXME: remove, this is for debug
-
-    pack_fft_2<<<local, local>>>(device_in, device_out);
-    cudaDeviceSynchronize();
-
-    // for (int row = 0; row < local/(p*b); row++) {
-    //     Complex* row_ptr = device_out + row * local * p * b;
-    //     cufftExecZ2Z(plan3, row_ptr, row_ptr, CUFFT_FORWARD);
-    // }
-
-    cudaMemcpy(host_out, device_out, local_size, cudaMemcpyDeviceToHost);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    double ns = duration.count();
-    if (id == 0) std::cout << "FFT: " << ns << " ns" << std::endl;
-
+    #ifdef __PRINT__RESULTS__
+    MPI_Barrier(MPI_COMM_WORLD);
     if (id == 0) {
         std::cout << "Host out" << std::endl;
-        for (int i = 0; i < local; i++) {
-            for (int j = 0; j < local; j++) {
-                std::cout << host_out[i * local + j].x << " ";
+        for (int i = 0; i < LOCAL_DIM; i++) {
+            for (int j = 0; j < LOCAL_DIM; j++) {
+                std::cout << host_buf1[i * LOCAL_DIM + j].x << " ";
             }
             std::cout << std::endl;
         }
-    }    
+    }
+    #endif
 
-    free(host_in);
-    free(host_out);
-    free(twiddles0);
-    free(twiddles1);
-    cudaFree(device_in);
-    cudaFree(device_out);
-
-    cufftDestroy(plan0);
-    cufftDestroy(plan1);
-    cufftDestroy(plan2);
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_DESTROY(plan0));
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_DESTROY(plan1));
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_DESTROY(plan2));
+    DEVICE_FFT_SAFE_CALL(DEVICE_FFT_DESTROY(plan3));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE_HOST(host_buf0));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE_HOST(host_buf1));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE_HOST(host_twiddles0));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE_HOST(host_twiddles1));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE(device_buf0));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE(device_buf1));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE(device_twiddles0));
+    DEVICE_RT_SAFE_CALL(DEVICE_FREE(device_twiddles1));
 
     MPI_Finalize();
 	
